f8f50dcce4e37d48d589a21ef4686ed33cf69075
cmwema <calwbmwema65@gmail.com>
2023-11-01 16:21:42 +0300

    allow fetching from the front end

--- erp/settings.py
+++ erp/settings.py
@@ -17,13 +17,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    'jcb.onrender.com'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -112,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
