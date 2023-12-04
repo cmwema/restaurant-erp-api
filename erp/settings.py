4a6c9cfab8a192c64f5cdd0a27789034a2b1c921
calebmwema65@gmail.com <calebmwema65@gmail.com>
2023-12-04 19:46:24 +0300

    deployment: render

--- erp/settings.py
+++ erp/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-j#*kx0ic%jmy^ndp#ncr*i2t!vqx@g_j5a2b9d=&tn&2+*6jpv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "jcbbbq.azurewebsites.net", "https://jcb.onrender.com/"]
 
