d8020e2b4dac4c0141c0ef77e6405bb30ff3410f
calebmwema65@gmail.com <calebmwema65@gmail.com>
2023-12-05 01:12:36 +0300

    total orders per product

--- core/serializers/ProductSerializer.py
+++ core/serializers/ProductSerializer.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from core.models.Product import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
     image_url = serializers.ImageField(required=False)
-    
+    total_orders = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = "__all__"
+
+    def get_total_orders(self, obj):
+        return obj.total_orders
